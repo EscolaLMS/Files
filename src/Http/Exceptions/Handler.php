cf3f5168fe839d60524ec59ba64d1af1a713ca1e
Mariusz `shd` Gliwiński <shd@nawia.net>
2021-05-07 10:15:32 +0200

    [exception-handler] register renders AccessDeniedException as JSON, report tries to report to sentry (#24)

--- src/Http/Exceptions/Handler.php
+++ src/Http/Exceptions/Handler.php
@@ -5,6 +5,7 @@ use EscolaLms\Files\Http\Exceptions\Contracts\Renderable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Throwable;
 
 class Handler extends \Illuminate\Foundation\Exceptions\Handler
@@ -27,5 +28,33 @@ class Handler extends \Illuminate\Foundation\Exceptions\Handler
         {
             return parent::render($request, $exception);
         }
+    }
+
+    /**
+     * Report or log an exception.
+     *
+     * @param Throwable $exception
+     * @return void
+     * @throws Throwable
+     */
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->renderable(function (AccessDeniedHttpException $exception, $request) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        });
     }
 }
