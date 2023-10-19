91fe88df4387068cf5a2f837833a44d1ca0872c5
Dawid Miklas <dawid.miklas@escolasoft.com>
2023-10-19 11:04:39 +0200

    xlsx and zip files support

--- src/config/files.php
+++ src/config/files.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'mimes' => env('FILES_MIMES', 'avi,mov,mp3,mp4,wmv,pdf,doc,docx,ppt,png,jpg,jpeg,gif,webp,svg'),
+    'mimes' => env('FILES_MIMES', 'avi,mov,mp3,mp4,wmv,pdf,doc,docx,ppt,png,jpg,jpeg,gif,webp,svg,xlsx,zip'),
 ];
